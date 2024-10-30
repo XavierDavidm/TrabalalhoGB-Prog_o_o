1fc39b43df05d14c79d79f77629327b0b8b2ac24
XavierDavidm <davidmoreirax15@hotmail.com>
2024-10-30 00:28:43 -0300

    1.1

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
+#entrgar diagrama de classes
+class Processo:
+    def __init__(self,pid):
+        self.pid=pid #pid é passado no main por um contador que sempre começa do ultimo número da fila
+                    #ex na fila de processos parou no 3 ele deve gerar ids apartir do 4 para cima
+    def execute(self):
+        pass
+class ProcessoDeCalculo(Processo):
+    pass
 
+
+
+
+
+
+
+
+
+
+
+
+#menu
