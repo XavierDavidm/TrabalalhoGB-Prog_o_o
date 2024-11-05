9d2c0572b550e540508be9afc7165e1d32a23371
XavierDavidm <davidmoreirax15@hotmail.com>
2024-11-05 20:23:10 -0300

    p1

--- test.py
+++ test.py
@@ -5,18 +5,71 @@ class Processo:
                     #ex na fila de processos parou no 3 ele deve gerar ids apartir do 4 para cima
     def execute(self):
         pass
-class ProcessoDeCalculo(Processo):
-    pass
+
+class ProcessoCalculo(Processo):
+    def __init__(self, pid):
+        super().__init__(pid)
+    def execute(self):
+        pass
+
+class ProcessoGravacao(Processo):
+    def __init__(self, pid):
+        super().__init__(pid)
+    def execute(self):
+        pass
+
+class ProcessoLeitura(Processo):
+    def __init__(self, pid):
+        super().__init__(pid)
+    def execute(self):
+        pass
+
+class ProcessoImpressao(Processo):
+    def __init__(self, pid):
+        super().__init__(pid)
+    def execute(self):
+        pass
 
 
-
-
-
-
-
-
-
-
-
-
+#main e sistema/menu
+fila=[]
+arq=open('computation.txt','w')
 #menu
+encerrar=False
+while encerrar!=True:
+    print('______________')
+    print('-----MENU-----')
+    print('1->Criar Processo')
+    print('2->Executar Próximo')
+    print('3->Executar Processo específico')
+    print('4->Salvar a fila de processos')
+    print('5->Carregar do arquvio a fila processos')
+    print('0->Salvar e Sair')
+    r=str(input('digite a ação que deseja realizar:'))
+    if r=='0':
+        encerrar=True
+        #chamar save aq
+        print('encerrando sistema...')
+
+    elif r=='1':
+        print('Tipos de processo')
+        print('1->Processo de Cálculo')
+        print('2->Processo de Gravação')
+        print('3->Processo de Leitura')
+        print('4->Processo de Impressão')
+        r=int(input())
+
+    elif r=='2':
+        pass
+
+    elif r=='3':
+        pass
+
+    elif r=='4':
+        pass
+
+    elif r=='5':
+        pass
+
+    else:
+        pass
