56a050472f8f34e4cddb60ee0c383e7647d47edc
XavierDavidm <davidmoreirax15@hotmail.com>
2024-11-19 17:47:32 -0300

    save v1

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
 #entrgar diagrama de classes
 class Processo:
-    def __init__(self,pid):
+    def __init__(self,pid,tipo):
         self.pid=int(pid) #pid é passado no main por um contador que sempre começa do ultimo número da fila
                     #ex na fila de processos parou no 3 ele deve gerar ids apartir do 4 para cima
+        self.tipo=str(tipo) #tipo identifica qual processo é('c','g','l','i')
     def execute(self):
         pass
     def getId(self):
         return self.pid
+    
 class ProcessoCalculo(Processo):
-    def __init__(self, pid, expressao): # expressao é uma string que será recebida e depois dividida usando split
-        super().__init__(pid)
-        expressao=str(expressao)#validação só para garantir que é string
-        i=expressao.split(' ',3) #"i" split dividirá a string em operandoA,operandoB e operador
-        
+    def __init__(self, pid, tipo, expressao): # expressao é uma string que será recebida e depois dividida usando split
+        super().__init__(pid,tipo)
+        self.expressao=str(expressao)#validação só para garantir que é string
+        i=self.expressao.split(' ',3) #"i" split dividirá a string em operandoA,operandoB e operador
         self.operandoA=int(i[0]) 
         self.operandoB=int(i[2])
         self.operador=i[1]
 
     def execute(self):
         #seletor de operação e calculadora
+        print('executando Cálculo...')
         if self.operador=='+':
             resultado=(self.operandoA+self.operandoB)
             print(self.operandoA,'+',self.operandoB,'é igual a',resultado)
@@ -35,25 +37,44 @@ class ProcessoCalculo(Processo):
             print('operação inválida')
 
 class ProcessoGravacao(Processo):
-    def __init__(self, pid, expressao):
-        super().__init__(pid)
+    def __init__(self, pid, tipo, expressao):
+        super().__init__(pid,tipo)
         self.expressao=str(expressao)
     def execute(self): #escreve expressão no fim do arquivo sem sobrescrever
+        print('executando Gravação...')
         with open('computation.txt','a') as ARQcomputation:
             linha=str(self.expressao+'\n')
             ARQcomputation.write(linha)
 
 class ProcessoLeitura(Processo):
-    def __init__(self, pid):
-        super().__init__(pid)
+    def __init__(self, pid, tipo):
+        super().__init__(pid,tipo)
     def execute(self):
-        pass
-
+        print('executando Leitura...')
+        #le o arquivo computation e retorna a fila com os processo lidos
+        with open('computation.txt','r') as ARQcomputation:
+            nLinhas=int(sum(1 for _ in ARQcomputation))
+            ARQcomputation.seek(0)
+            filaTemp=[] #fila temporaria que será adicionada a fila oficial dentro do sistema
+            for i in range(nLinhas):
+                linha = ARQcomputation.readline().strip()
+                processo=ProcessoCalculo(sistema.geradorDeIds(),'c',linha)
+                filaTemp.append(processo)
+            print(len(filaTemp))
+        #limpa o arquivo computation
+        with open('computation.txt','w') as ARQcomputation:
+            pass
+        return sistema.fila.extend(filaTemp)
+    
 class ProcessoImpressao(Processo):
-    def __init__(self, pid):
-        super().__init__(pid)
+    def __init__(self, pid, tipo):
+        super().__init__(pid, tipo)
     def execute(self):
-        pass
+        print('executando Impressão...')
+        for i in range(len(sistema.fila)):
+            processo=sistema.fila[i]
+            if processo.tipo=='c':
+                print('pID:',processo.getId(),'- tipo: Cálculo','expressão:',processo.expressao)
 
 class Sistema:
     def __init__(self):
@@ -93,19 +114,30 @@ class Sistema:
                     if operador=='/' and operandoB == 0:
                         print('Erro! é impossivel dividir por zero')
                     else:
-                        Processo=ProcessoCalculo(self.geradorDeIds(),expressao)
+                        Processo=ProcessoCalculo(self.geradorDeIds(),'c',expressao)
+                        print('Id:',Processo.getId())
                         self.fila.append(Processo)
+                        print('Processo de Cálculo criado com sucesso!')
                     
                 elif r=='2': #Processo de Gravação
                     expressao=str(input('Digite a expressão que será gravada (usar espaço)ex :1 + 1 :'))
-                    Processo=ProcessoGravacao(self.geradorDeIds(),expressao)
+                    Processo=ProcessoGravacao(self.geradorDeIds(),'g',expressao)
+                    print('Id:',Processo.getId())
                     self.fila.append(Processo)
+                    print('Processo de Gravação criado com sucesso!')
 
                 elif r=='3': #Processo de Leitura
-                    pass
+                    Processo=ProcessoLeitura(self.geradorDeIds(),'l')
+                    print('Id:',Processo.getId())
+                    self.fila.append(Processo)
+                    print('Processo de Leitura criado com sucesso!')
 
                 elif r=='4': #Processo de Impressão
-                    pass
+                    Processo=ProcessoImpressao(self.geradorDeIds(),'i')
+                    print('Id:',Processo.getId())
+                    self.fila.append(Processo)
+                    print('Processo de Impressão criado com sucesso!')
+
                 else:
                     print('seleção inválida!')
 
@@ -138,41 +170,62 @@ class Sistema:
                         print('Processo não encontrado na lista')
             #Salvar a fila de processos                 
             elif r=='4':
-                pass
+                with open('fila.txt','r') as ArqFila:
+                    for processo in self.fila:
+                        ArqFila.write(f"{processo.tipo},{processo.pid}")
+                        
+
+                    print('salvando fila atual em arquivo...')
+
             #Carregar do arquivo a fila de processos #mudar para acomodar o arquivo da fila
             elif r=='5':
                 print('Carregando Fila de Processos salva...')
-                with open('computation.txt','r') as ARQcomputation:
-                    nLinhas=int(sum(1 for _ in ARQcomputation))
-                    ARQcomputation.seek(0)
+                with open('fila.txt','r') as ArqFila:
+                    nLinhas=int(sum(1 for _ in ArqFila))
+                    ArqFila.seek(0)
                     for i in range(nLinhas):
-                        linha = ARQcomputation.readline().strip()
-                        Processo=ProcessoCalculo(self.geradorDeIds(),linha)
-                        self.fila.append(Processo)
+                        linha = ArqFila.readline().strip()
+                        a = linha.split(',',3) #a[0]=tipo a[1]=pid a[2]=expressao(se tiver)
+                        if a[0]=='c':
+                            Processo=ProcessoCalculo(a[1],'c',a[2])
+                            self.fila.append(Processo)
+                        elif a[0]=='g':
+                            Processo=ProcessoGravacao(a[1],'g',a[2])
+                            self.fila.append(Processo)
+                        elif a[0]=='l':
+                            Processo=ProcessoLeitura(a[1],'l')
+                            self.fila.append(Processo)
+                        elif a[0]=='i':
+                            Processo=ProcessoImpressao(a[1],'i')
+                            self.fila.append(Processo)
             else:   
                 print('comando não reconhecido!')
     #este metodo gera os ids, é usado pelos criadores de processo e pelo carregador       
     def geradorDeIds(self): 
-        self.newId=self.fila[-1].getId+1 #mudar para o ultimo da fila
+        if len(self.fila)==0:
+            self.newId=1
+        else:
+            processo=self.fila[-1]
+            self.newId=(processo.getId()) + 1 #mudar para o ultimo da fila
         return(self.newId)
 
 #anotações para fazer o trabalho: (apagar depois)
 #check list
 #processo de calculo + execute ->FEITO
 #processo de GRAVAÇÃO + execute -> feito
-#processo de leitura + execute ->
-#processo de impressao + execute ->
+#processo de leitura + execute -> feito
+#processo de impressao + execute -> feito
 
 #menu
 #1.1-> FEITO
 #1.2-> feito
-#1.3->
-#1.4->
+#1.3-> feito
+#1.4-> feito
 
 #2-> FEITO
-#3->feito
-#4->
-#5->
+#3-> feito
+#4-> 
+#5-> feito
 
 #implementar save
 #implementar validações adicionais no 1(criação dos processos)
